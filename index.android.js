6453b5e05de88b90a340fbd1b23e5fed892b3290
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2017-08-16 00:56:29 +0500

    firebase config removed

--- index.android.js
+++ index.android.js
@@ -1,14 +1,4 @@
 import './App/Config/ReactotronConfig'
 import { AppRegistry } from 'react-native'
 import App from './App/Containers/App'
-import firebase from 'firebase'
-var config = {
-    apiKey: "AIzaSyBPjJjMKucWoKhl2Kjfm4UfYvIehgEHIMU",
-    authDomain: "touristapp-4dc75.firebaseapp.com",
-    databaseURL: "https://touristapp-4dc75.firebaseio.com",
-    projectId: "touristapp-4dc75",
-    storageBucket: "touristapp-4dc75.appspot.com",
-    messagingSenderId: "378627312242"
-  };
-firebase.initializeApp(config);
 AppRegistry.registerComponent('TouristApp', () => App)
